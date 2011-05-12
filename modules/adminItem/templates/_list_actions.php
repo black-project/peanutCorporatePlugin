425e85398b4ff86ca013d16839943fd5c2015ed6
Alexandre pocky Balmes <albalmes@gmail.com>
2011-05-12 12:59:25 +0200

    Ajout du template _list_actions pour gérer correctement les ajouts d'items

---
+++ modules/adminItem/templates/_list_actions.php
@@ -0,0 +1,6 @@
+<li class="sf_admin_action_create new page">
+  <?php echo link_to(__('Create new page', array(), 'messages'), 'adminPage/new', array()) ?>
+</li>
+<li class="sf_admin_action_create new link">
+  <?php echo link_to(__('Create new link', array(), 'messages'), 'adminLink/new', array()) ?>
+</li>
